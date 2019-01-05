d4f2ac008c50c6df68b0dccbdf2fea675fd233f5
Michael Miles <michael.gage.miles@gmail.com>
2019-01-05 15:16:02 -0800

    Initial Commit

---
+++ CoreDataSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataSwift
+//
+//  Created by Michael Miles on 1/5/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
